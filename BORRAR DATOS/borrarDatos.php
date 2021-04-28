1aefdb386d5d9f36548a2039b969900e4f18322a
Jose Antonio <joseantoniomarquezglez@gmail.com>
2021-04-28 09:19:16 +0200

    Actualizacion Delete
    Actualizacion del ejercicio delete.

--- BORRAR DATOS/borrarDatos.php
+++ BORRAR DATOS/borrarDatos.php
@@ -1,19 +1,19 @@
 <html>
 
 <head>
-  <title>Problema</title>
+  <title>DELETE</title>
 </head>
 
 <body>
   <?php
-  $conexion = mysqli_connect("localhost", "root", "", "base1") or
+  $conexion = mysqli_connect("localhost", "fuesdawy", "Nilocamilalinda7", "fuesdawy_Clientes") or
     die("Problemas con la conexión");
 
-  $registros = mysqli_query($conexion, "select codigo from alumnos
-                        where mail='$_REQUEST[mail]'") or
+  $registros = mysqli_query($conexion, "select correo from clientes
+                        where correo='$_REQUEST[correo]'") or
     die("Problemas en el select:" . mysqli_error($conexion));
   if ($reg = mysqli_fetch_array($registros)) {
-    mysqli_query($conexion, "delete from alumnos where mail='$_REQUEST[mail]'") or
+    mysqli_query($conexion, "delete from clientes where correo='$_REQUEST[correo]'") or
       die("Problemas en el select:" . mysqli_error($conexion));
     echo "Se efectuó el borrado del alumno con dicho mail.";
   } else {
